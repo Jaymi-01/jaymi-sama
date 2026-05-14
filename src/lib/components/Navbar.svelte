42ccff67b04c0a58d1f49bf9a202dabee19b94b5
Joel Miller <millerjoel7597@gmail.com>
2026-05-14 18:45:38 +0100

    Fix navRef check in onMount for GSAP animation

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -12,13 +12,15 @@
   }
 
   onMount(() => {
-    gsap.from(navRef, {
-      y: -100,
-      opacity: 0,
-      duration: 1,
-      ease: 'power3.out',
-      delay: 0.5
-    });
+    if (navRef) {
+      gsap.from(navRef, {
+        y: -100,
+        opacity: 0,
+        duration: 1,
+        ease: 'power3.out',
+        delay: 0.5
+      });
+    }
   });
 
   const links = [
